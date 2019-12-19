91ce55d3ab5916c637d4d34e06a613124ed094d9
joan cruz <joancruz95@gmail.com>
2019-12-19 01:03:11 +0000

    Add a new public instance method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,12 +20,15 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
-        return self.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            return [obj for obj in self.__objects if type(obj) is cls]
 
     def new(self, obj):
         """sets __object to given obj
@@ -55,3 +58,10 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects"""
+        if isinstance(obj, BaseModel):
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
