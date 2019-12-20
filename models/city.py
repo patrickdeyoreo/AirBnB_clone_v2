f807bf842154508cb6c894a6e380baf156aa0675
Joan Cruz <joancruz95@gmail.com>
2019-12-19 17:56:42 -0800

    Updated Place for SQLAlchemy with relationships to City and User

--- models/city.py
+++ models/city.py
@@ -10,10 +10,15 @@ class City(BaseModel, Base):
     Attributes:
         state_id: The state id
         name: input name
+        state: relationship between a state and cities
+        places: relationship between a city and places
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
     state = relationship(
         'State', back_populates='cities'
+    )
+    places = relationship(
+        'Place', back_populates='cities', cascade='all, delete-orphan'
     )
