150abde8d9122dcc256ee7dddaa840fcb863769a
Joan Cruz <joancruz95@gmail.com>
2019-12-20 14:44:39 -0800

    Using relative imports in models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,14 +3,14 @@
 import os
 
 import models
-from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-from models.engine import db_storage, file_storage
+from . base_model import BaseModel
+from . amenity import Amenity
+from . city import City
+from . place import Place
+from . review import Review
+from . state import State
+from . user import User
+from . engine import db_storage, file_storage
 
 if os.getenv('HBNB_TYPE_STORAGE') == 'db':
     storage = db_storage.DBStorage()
