20920eb02718782c4dc40aa0e4febf91447a9a4c
patrickdeyoreo <pdeyoreo@gmail.com>
2020-01-03 23:21:16 -0800

    Adding solutions to task 1 & 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,7 +3,7 @@
 Create a tarball containing the contents of web_static
 """
 
-from os import path
+from os.path import isfile, join
 from shlex import quote
 from time import strftime
 from fabric.api import local
@@ -14,7 +14,7 @@ def do_pack():
     Archive the contents of web_static
     """
     now = strftime('%Y%m%d%H%M%S')
-    tgz = path.join('versions', 'web_static_{}.tgz'.format(now))
+    tgz = join('versions', 'web_static_{}.tgz'.format(now))
     local('mkdir -p versions')
     local('tar -czf {} web_static'.format(quote(tgz)))
-    return tgz if path.isfile(tgz) else None
+    return tgz if isfile(tgz) else None
