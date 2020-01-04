068aa8070cb94d42656cef0905c2930a3156aaed
patrickdeyoreo <pdeyoreo@gmail.com>
2020-01-04 01:56:37 -0800

    Inserting instead of appending with sed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,6 +23,6 @@ ln -fns /data/web_static/releases/test /data/web_static/current
 
 chown -hR ubuntu:ubuntu /data
 
-sed -i '/^\tlisten 80 default_server;$/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sed -i '/^\tlisten 80 default_server;$/i location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 
 service nginx restart
