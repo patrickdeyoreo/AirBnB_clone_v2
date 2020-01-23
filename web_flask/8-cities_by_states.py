e99234c4bb415658234b051b9ccac26c42b81f70
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-01-22 22:25:15 -0800

    Changed 8-cities_by_states template to use state.cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,16 +11,6 @@ HOST = '0.0.0.0'
 PORT = '5000'
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    '''
-    list states
-    '''
-    TEMPLATE = '7-states_list.html'
-    STATES = storage.all(State).values()
-    return render_template(TEMPLATE, states=STATES)
-
-
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     '''
@@ -28,8 +18,7 @@ def cities_by_states():
     '''
     TEMPLATE = '8-cities_by_states.html'
     STATES = storage.all(State).values()
-    STATE_CITIES = {state.id: state.cities for state in STATES}
-    return render_template(TEMPLATE, states=STATES, state_cities=STATE_CITIES)
+    return render_template(TEMPLATE, states=STATES)
 
 
 if __name__ == '__main__':
